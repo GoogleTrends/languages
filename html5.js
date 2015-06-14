efb4fcb5d271cd9789b658ec96c53cf96c0d88f6
Kinross <d_kinross@hotmail.com>
2015-06-14 22:47:46 +0100

    ie9 message

--- html5.js
+++ html5.js
@@ -31,6 +31,30 @@
             document.body.innerHTML = htmlMessage;
         }
     }
+	
+	
+    
+    /**************************************************************
+     *
+     *				IE9
+     *
+     *************************************************************/
+	
+    var ie = (function(){
+    
+        var undef, v = 3, div = document.createElement('div'), all = div.getElementsByTagName('i');
+        
+        while (div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->', all[0]) ;
+        
+        return v > 4 ? v : undef;
+        
+    }());
+    
+    if (ie < 10) {
+        window.onload = function(){
+            document.body.innerHTML = htmlMessage;
+        }
+    }
     
     /**************************************************************
      *
